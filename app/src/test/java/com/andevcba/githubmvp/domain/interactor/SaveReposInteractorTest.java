8c0c4bc570f4a06d5ec1eb9f9bd557f3d0e11074
Lucas Nobile <lucasnrb@gmail.com>
2016-10-14 18:00:21 -0300

    Add unit test for SaveReposInteractor.

---
+++ app/src/test/java/com/andevcba/githubmvp/domain/interactor/SaveReposInteractorTest.java
@@ -0,0 +1,76 @@
+package com.andevcba.githubmvp.domain.interactor;
+
+import com.andevcba.githubmvp.data.model.Repo;
+import com.andevcba.githubmvp.data.model.ReposByUsername;
+import com.andevcba.githubmvp.data.repository.InMemoryRepository;
+import com.andevcba.githubmvp.data.repository.ReposCache;
+import com.andevcba.githubmvp.data.repository.ReposCallback;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Unit test for {@link SaveReposInteractor}.
+ *
+ * @author lucas.nobile
+ */
+public class SaveReposInteractorTest {
+
+    private final static String USERNAME = "AnDevCba";
+    private final static String NOT_IN_CACHE_USERNAME = "NotInCache";
+
+    private final static Repo REPO1 = new Repo("repo1", "url1");
+    private final static Repo REPO2 = new Repo("repo2", "url2");
+    private static List<Repo> REPO_LIST = new ArrayList<>();
+
+    private static TreeMap<String, List<Repo>> REPOS_BY_USERNAME_MAP;
+    private static ReposByUsername reposByUsername;
+
+    @Mock
+    private ReposCallback reposCallback;
+
+    @Mock
+    private ReposCache reposCache;
+
+    @InjectMocks
+    private InMemoryRepository repository;
+
+    @InjectMocks
+    private SaveReposInteractor interactor;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        // Mocked repo list
+        REPO_LIST.add(REPO1);
+        REPO_LIST.add(REPO2);
+
+        // Mocked repos by username
+        REPOS_BY_USERNAME_MAP = new TreeMap<>();
+        REPOS_BY_USERNAME_MAP.put(USERNAME, REPO_LIST);
+
+        reposByUsername = new ReposByUsername(REPOS_BY_USERNAME_MAP, true /* is cached */);
+    }
+
+    @Test
+    public void execute_shouldSaveReposByUsername() {
+        // When
+        repository.saveReposByUsername(reposByUsername);
+
+        // Then
+        verify(reposCache, times(1)).put(USERNAME, REPO_LIST);
+    }
+}
